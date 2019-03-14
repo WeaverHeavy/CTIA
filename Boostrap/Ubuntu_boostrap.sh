7e61fd23b3ce2ab1ab5079a7ed4c40827e782146
Weaver <1121694-whilabs@users.noreply.gitlab.com>
2019-03-14 18:17:30 +0000

    Update Ubuntu_boostrap.sh

--- Boostrap/Ubuntu_boostrap.sh
+++ Boostrap/Ubuntu_boostrap.sh
@@ -5,3 +5,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
 sudo apt update
 sudo apt-get install -y docker-ce
+exit
