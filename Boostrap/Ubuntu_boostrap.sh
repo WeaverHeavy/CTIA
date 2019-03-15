0b2dc27ec47bf311a83b494c2e416e174404bc2a
WHI <1121694-whilabs@users.noreply.gitlab.com>
2019-03-15 15:20:56 +0000

    Update Boostrap/Ubuntu_boostrap.sh

--- Boostrap/Ubuntu_boostrap.sh
+++ Boostrap/Ubuntu_boostrap.sh
@@ -4,8 +4,9 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge"
 sudo apt update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-compose
 sudo usermod -aG docker ubuntu
 sudo docker volume create portainer_data
-sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer 
+#consider tagging the container with a easily recognizable name
 exit
