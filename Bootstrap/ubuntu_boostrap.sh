9c969acc4d94c7cd90eecbf3861969c802e779a2
WHI <1121694-whilabs@users.noreply.gitlab.com>
2019-03-20 19:50:24 +0000

    Update Bootstrap/ubuntu_boostrap.sh

--- Bootstrap/ubuntu_boostrap.sh
+++ Bootstrap/ubuntu_boostrap.sh
@@ -9,5 +9,6 @@ sudo usermod -aG docker ubuntu
 sudo docker volume create portainer_data
 sudo docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer 
 #consider tagging the container with a easily recognizable name
+#if this isn't set elastisearch might crash
 sudo sysctl -w vm.max_map_count=262144
 exit
